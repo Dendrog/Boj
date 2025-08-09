2776e4439317a1ce26442ec8e03c0bb478dda06f
Dendrog <goodshs335@gmail.com>
2025-08-10 04:57:14 +0900

    2743.cpp

---
+++ marathon/2743.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+using graph = vector<vector<int>>;
+int main(){
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    string st;
+    cin >> st;
+    cout << st.length();
+}
